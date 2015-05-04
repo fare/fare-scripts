9e36d64f33056df4f7409d3ebca6eb09502f1379
Francois-Rene Rideau <tunes@google.com>
2015-05-04 11:56:49 -0400

    Migrate a couple functions from my zsh aliases.

--- shell-aliases.lisp
+++ shell-aliases.lisp
@@ -1,19 +1,18 @@
-;; Migrating my zsh aliases here...
+;; Slowly migrating my zsh aliases here...
 
 (defpackage :fare-scripts/shell-aliases
-  (:use :cl :uiop :inferior-shell :optima :optima.ppcre :cl-launch/dispatch)
-  (:export #:mkba #:mkba2 #:fare-scripts-symlinks))
+  (:use :cl :fare-utils :uiop :inferior-shell :optima :optima.ppcre :cl-launch/dispatch))
 
 (in-package :fare-scripts/shell-aliases)
 
-(defun mkba ()
+(def*fun mkba ()
   (with-current-directory ((subpathname (user-homedir-pathname) "src/fare/bastiat.org/"))
     (run '(make dep)) (run '(make))))
 
-(defun mkba2 ()
+(def*fun mkba2 ()
   (mkba) (mkba))
 
-(defun fare-scripts-symlinks ()
+(def*fun fare-scripts-symlinks ()
   (let ((binarch (resolve-absolute-location `(,(getenv "BINDIR") ,(getenv "BINARCH")) :ensure-directory t)))
     (with-current-directory (binarch)
       (dolist (i (cl-launch/dispatch:all-entry-names))
