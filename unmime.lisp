dcb8eee99b8158c2205071108408a2226ea9001e
Francois-Rene Rideau <fare@tunes.org>
2016-07-31 21:32:10 -0400

    unmime: trivially process multimime.

--- unmime.lisp
+++ unmime.lisp
@@ -9,22 +9,48 @@
   (format stream "usage: unmime | unmime - | unmime <filename>~%~
 	decodes the specified file (default: stdin) as a single-file mime container~%"))
 
+(defun get-parsed-mime (x)
+  (etypecase x
+    ((or stream string) (cl-mime:parse-mime x))
+    (pathname (with-input-file (s x) (cl-mime:parse-mime s)))))
+
+(defun decode-text-mime (tm)
+  (let* ((c (coerce (decode-content tm) '(vector (unsigned-byte 8) *)))
+	 (cs (first (charset tm)))
+	 (e (babel-encodings:get-character-encoding
+	     (and cs (find-symbol (string-upcase cs) :keyword)))))
+    (octets-to-string c :encoding e)))
+
 (defun get-mime-string (x)
-  (let* ((m (etypecase x
-              ((or stream string) (cl-mime:parse-mime x))
-              (pathname (with-input-file (s x) (cl-mime:parse-mime s)))))
+  (let* ((m (get-parsed-mime x))
          (tm (etypecase m
                (text-mime m)
                (multipart-mime (let ((f (first (content m))))
                                  (check-type f text-mime)
-                                 f))))
-         (c (coerce (decode-content tm) '(vector (unsigned-byte 8) *)))
-         (cs (first (charset tm)))
-         (e (babel-encodings:get-character-encoding
-             (and cs (find-symbol (string-upcase cs) :keyword)))))
-    (octets-to-string c :encoding e)))
+                                 f)))))
+    (decode-text-mime tm)))
 
 (defun unmime (x) (princ (get-mime-string x)))
+
+(defun unmimeall (input output-name)
+  (let ((m (get-parsed-mime input)))
+    (unmime-to m output-name)))
+
+(defun unmime-to (m output-name)
+  (etypecase m
+    (text-mime
+     (format! t "Creating text file ~A~%" output-name)
+     (with-output-file (o output-name :if-exists :rename-and-delete)
+       (princ (decode-text-mime m) o)))
+    (multipart-mime
+     (loop
+       :for i :from 0
+       :for n :in (content m)
+       :do (unmime-to n (format nil "~A.~D" output-name i))))
+    (mime
+     (format! t "Creating binary file ~A~%" output-name)
+     (with-output-file (o output-name :element-type '(unsigned-byte 8) :if-exists :rename-and-delete)
+       (write-sequence (decode-content m) o)))))
 
 (defun main (argv)
   (match argv
@@ -35,4 +61,8 @@
        ((or "-h" "--help" "-?") (show-usage *standard-output*))
        ((ppcre "^-") (show-usage *error-output*))
        (_ (unmime (parse-native-namestring x)))))
+    ((list in out)
+     (unmimeall
+      (if (equal in "-") *standard-input* (parse-native-namestring in))
+      out))
     (_ (show-usage *error-output*))))
