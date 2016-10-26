7a09717015194ff7286003471e4c198165e5b131
Francois-Rene Rideau <fare@tunes.org>
2016-10-25 22:43:57 -0400

    make-multi.sh: Exclude workout-timer if required DLLs absent.

--- make-multi.sh
+++ make-multi.sh
@@ -1,7 +1,18 @@
-#!/bin/zsh
+#!/bin/zsh -eux
+
+has_dll () {
+    [[ -n "$(ldconfig -p 2> /dev/null | grep -F ${1}.so)" ]]
+}
+
+if has_dll libasound && has_dll libvorbis ; then
+    workout_timer=(--dispatch-system workout-timer)
+else
+    workout_timer=
+fi
+
 A=(
   cl-launch
-  --output $BINDIR/$BINARCH/multi --dump !
+  --output ${BINDIR}/${BINARCH}/multi --dump !
   --lisp sbcl
   --quicklisp
   --dispatch-system exscribe --system exscribe/typeset # add-on to exscribe
@@ -9,10 +20,10 @@ A=(
   --dispatch-system fare-scripts/toggle-touchpad
   --dispatch-system fare-scripts/unmime
   --dispatch-system tthsum
-  --dispatch-system workout-timer
+  ${workout_timer}
   --system fare-scripts # Many of its subsystems register their own functions!
   --system-package lisp-stripper --dispatch-entry lispwc
 )
 $A $@
-multi fare-scripts-symlinks
+multi fare-scripts-symlink
 multi help
