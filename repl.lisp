21bb49141f2aa1fd8c20e2dd1aff3c747527400c
Francois-Rene Rideau <fare@tunes.org>
2020-01-01 18:56:21 -0500

    Trivial REP and REPL from the command-line.

--- repl.lisp
+++ repl.lisp
@@ -1,7 +1,8 @@
 ;;; REPL utilities
 (uiop:define-package :fare-scripts/repl
-  (:use :cl :fare-utils :uiop :inferior-shell :optima :optima.ppcre :cl-scripting)
-  (:import-from :swank))
+  (:use :cl :fare-utils :uiop :inferior-shell :optima :optima.ppcre :cl-scripting :cl-launch/dispatch)
+  (:import-from :swank)
+  (:export #:read-eval-print #:rep))
 
 (in-package :fare-scripts/repl)
 
@@ -10,3 +11,20 @@
 (push (lambda (&rest args) (apply #'swank:ed-in-emacs args) t) sb-ext:*ed-functions*)
 #+ccl
 (setq ccl:*resident-editor-hook* #'swank:ed-in-emacs)
+
+(exporting-definitions
+(defun read-eval-print (s &optional (package :fare-scripts/repl))
+  (with-standard-io-syntax
+    (let ((*package* (find-package (standard-case-symbol-name package))))
+      (format t "~W~%" (eval-input s)))))
+
+(defun rep (a &optional b)
+  (if b (read-eval-print b a) (read-eval-print a))))
+(register-commands :fare-scripts/repl)
+
+(eval-when (:compile-toplevel)
+  (uiop:println "COMPILE-TOPLEVEL"))
+(eval-when (:load-toplevel)
+  (uiop:println "LOAD-TOPLEVEL"))
+(eval-when (:execute)
+  (uiop:println "EXECUTE"))
